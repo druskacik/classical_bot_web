94691ec49b78bd4ab430917bc0b2bc1e197fc716
Róbert Druska <robert.druska@gmail.com>
2026-08-16 15:52:46 +0200

    compress public assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  nitro: { compressPublicAssets: true },
   modules: ['@nuxt/ui', '@nuxt/content'],
   css: ['~/assets/css/main.css'],
   colorMode: {
