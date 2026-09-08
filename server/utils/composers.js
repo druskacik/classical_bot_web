33c73d4e1f3558ddd9309e7d77ec5db367fe6421
Róbert Druska <robert.druska@gmail.com>
2026-09-08 23:41:36 +0200

    new composer covers

--- server/utils/composers.js
+++ server/utils/composers.js
@@ -2,7 +2,7 @@ import knex from '#layers/concerts/server/utils/connection.js'
 import { composerPath, seasonLabel, catalogueLabel } from '#shared/utils/composers.js'
 import { publishedPlaylists, composerCounts, rankedWorks } from './composer-queries.js'
 
-const coverComposers = new Set([1, 2, 3, 4, 5, 6, 7, 14, 20, 29])
+const coverComposers = new Set([1, 2, 3, 4, 5, 6, 7, 8, 11, 12, 14, 16, 18, 20, 29, 34, 39, 41, 49, 67])
 
 export const getComposerDirectory = defineCachedFunction(async () => {
   const playlists = await publishedPlaylists(knex)
